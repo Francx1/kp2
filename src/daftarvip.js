0d68ec7ca5b42674021461d962adb8ba1ed19deb
Francx <77581257+Francx1@users.noreply.github.com>
2021-02-20 22:31:38 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,15 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/554398009628*${prefix}owner*_`
+}
+exports.daftarvip = daftarvip
---
+++ src/help1.js
@@ -0,0 +1,9 @@
+const help1 = (prefix) => {
+	return `
+┃MENU EM ATUALIZAÇÃO ⚠️
+┣QUALQUER COISA
+┣CHAME O DONO
+┣wa.me/+554398009628`
+}
+exports.help1 = help1
+
